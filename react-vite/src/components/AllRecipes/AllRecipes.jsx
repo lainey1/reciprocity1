fe16ec08a0e831e99b2f20858c56f3f4bbdfa5bf
lainey1 <lainey.d.paddock@gmail.com>
2025-01-20 03:02:03 -0800

    starting add/save recipe to collection

--- react-vite/src/components/AllRecipes/AllRecipes.jsx
+++ react-vite/src/components/AllRecipes/AllRecipes.jsx
@@ -1,52 +1,157 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { thunkFetchRecipes } from "../../redux/recipes";
 import no_image_available from "../../../public/no_image_available.png";
+import { addRecipe, fetchCollectionsByOwner } from "../../redux/collections";
 
 import "./AllRecipes.css";
 
 function AllRecipes() {
   const dispatch = useDispatch();
   const recipes = useSelector((state) => state.recipes?.recipes);
+  const userCollections = useSelector(
+    (state) => state.collections.ownerCollections
+  );
+  const userId = useSelector((state) => state.session.user.id);
+
   const recipesArr = Object.values(recipes);
+
+  // Store selected collections by recipe ID (as arrays)
+  const [selectedCollections, setSelectedCollections] = useState({});
+  const initialFetchCompleted = useRef(false);
 
   useEffect(() => {
     dispatch(thunkFetchRecipes());
-  }, [dispatch]);
+    dispatch(fetchCollectionsByOwner(userId));
+  }, [dispatch, userId]);
+
+  useEffect(() => {
+    // Only update selectedCollections once after the initial fetch
+    if (userCollections.length > 0 && !initialFetchCompleted.current) {
+      const updatedSelectedCollections = {};
+      recipesArr.forEach((recipe) => {
+        const existingCollections = userCollections
+          .filter((collection) =>
+            collection?.recipes?.some((r) => r.id === recipe.id)
+          )
+          .map((collection) => collection.id);
+
+        if (existingCollections.length > 0) {
+          updatedSelectedCollections[recipe.id] = existingCollections;
+        }
+      });
+
+      setSelectedCollections((prevState) => {
+        const hasChanges = Object.keys(updatedSelectedCollections).some(
+          (key) =>
+            updatedSelectedCollections[key].toString() !==
+            prevState[key]?.toString()
+        );
+        if (hasChanges) {
+          return updatedSelectedCollections;
+        }
+        return prevState;
+      });
+
+      initialFetchCompleted.current = true;
+    }
+  }, [userCollections, recipesArr]);
+
+  const handleCollectionChange = (recipeId, collectionId, isChecked) => {
+    setSelectedCollections((prevState) => {
+      const updatedSelections = prevState[recipeId] || [];
+
+      if (isChecked) {
+        // Add collection ID to selected collections
+        updatedSelections.push(collectionId);
+      } else {
+        // Remove collection ID from selected collections
+        const index = updatedSelections.indexOf(collectionId);
+        if (index > -1) {
+          updatedSelections.splice(index, 1);
+        }
+      }
+
+      return {
+        ...prevState,
+        [recipeId]: updatedSelections,
+      };
+    });
+  };
+
+  const handleSaveRecipe = (recipeId) => {
+    const collectionIds = selectedCollections[recipeId];
+    if (!collectionIds || collectionIds.length === 0) {
+      alert("Please select at least one collection to save this recipe.");
+      return;
+    }
+    // Save to each selected collection
+    collectionIds.forEach((collectionId) => {
+      dispatch(addRecipe(collectionId, recipeId));
+    });
+  };
 
   return (
     <div id="all-recipes-page-container">
       <div className="all-recipes-grid">
-        {recipesArr.map((recipe) => {
-          return (
-            <div key={recipe.id} className="recipe-tile">
-              <Link to={`${recipe.id}`} className="all-recipes-link">
-                <div className="recipe-image-container">
-                  {recipe.preview_image ? (
-                    <img
-                      src={recipe.preview_image}
-                      alt={`${recipe.name} image`}
-                      className="recipe-image"
-                    />
-                  ) : (
-                    <img
-                      src={no_image_available}
-                      alt="no image available"
-                      className="recipe-image"
-                    />
-                  )}
-                </div>
-                <div className="recipe-highlight">
-                  <p className="recipe-description">
-                    <span style={{ fontWeight: "bold" }}>{recipe.name}</span>{" "}
-                    {recipe.short_description}
-                  </p>
-                </div>
-              </Link>
+        {recipesArr.map((recipe) => (
+          <div key={recipe.id} className="recipe-tile">
+            <Link to={`${recipe.id}`} className="all-recipes-link">
+              <div className="recipe-image-container">
+                {recipe.preview_image ? (
+                  <img
+                    src={recipe.preview_image}
+                    alt={`${recipe.name} image`}
+                    className="recipe-image"
+                  />
+                ) : (
+                  <img
+                    src={no_image_available}
+                    alt="no image available"
+                    className="recipe-image"
+                  />
+                )}
+              </div>
+              <div className="recipe-highlight">
+                <p className="recipe-description">
+                  <span style={{ fontWeight: "bold" }}>{recipe.name}</span>{" "}
+                  {recipe.short_description}
+                </p>
+              </div>
+            </Link>
+            {/* Save button and collection selection */}
+            <div className="save-recipe-container">
+              <div>
+                <p>Select Collections:</p>
+                {userCollections?.map((collection) => (
+                  <div key={collection.id}>
+                    <label>
+                      <input
+                        type="checkbox"
+                        value={collection.id}
+                        checked={selectedCollections[recipe.id]?.includes(
+                          collection.id
+                        )}
+                        onChange={(e) =>
+                          handleCollectionChange(
+                            recipe.id,
+                            collection.id,
+                            e.target.checked
+                          )
+                        }
+                      />
+                      {collection.name}
+                    </label>
+                  </div>
+                ))}
+              </div>
+              <button onClick={() => handleSaveRecipe(recipe.id)}>
+                Save to Collection
+              </button>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
